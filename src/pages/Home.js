e84fbfc562675c59f3f87ee6f943d57c712e8747
ted <ted@warpedpuppy.com>
2018-03-24 16:03:56 -0700

    home page change

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,7 +18,7 @@ class Home extends React.Component {
 			<HomeCanvas />
 			<div className="textCont">
 			<p>Hello!</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas efficitur tellus velit, sit amet mollis felis porttitor nec. Aenean lorem turpis, tempus vitae ullamcorper quis, feugiat at magna. Donec leo quam, facilisis varius lacinia at, viverra ac enim. Proin tempor, lacus et molestie tincidunt, urna elit accumsan nisi, ac malesuada arcu leo ut velit.</p>
+            <p>Lorem ipsum dolor dsit amet, consectetur adipiscing elit. Maecenas efficitur tellus velit, sit amet mollis felis porttitor nec. Aenean lorem turpis, tempus vitae ullamcorper quis, feugiat at magna. Donec leo quam, facilisis varius lacinia at, viverra ac enim. Proin tempor, lacus et molestie tincidunt, urna elit accumsan nisi, ac malesuada arcu leo ut velit.</p>
 
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas efficitur tellus velit, sit amet mollis felis porttitor nec. Aenean lorem turpis, tempus vitae ullamcorper quis, feugiat at magna. Donec leo quam, facilisis varius lacinia at, viverra ac enim. Proin tempor, lacus et molestie tincidunt, urna elit accumsan nisi, ac malesuada arcu leo ut velit.</p>
 		      <Logo w="250" rotate="rotate(15)" color={true} />
