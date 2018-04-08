db153faa032f930ffc7fe7ceb3bfaecd2390832d
ted <ted@warpedpuppy.com>
2018-04-08 12:51:44 -0700

    contact page

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -14,18 +14,19 @@ export default class ContactForm extends React.Component {
 		    console.log(response);
 		})
 		.catch(function (error) {
-		    console.error(error);
+		    console.error('error = ', error);
 		});
 	}
 	render () {
 		return (
 			
-			<form onSubmit={(e) => this.onFormSubmit(e)}>
+			<form method="POST" action="//formspree.io/wp@tugtug.com">
 				<input type="email" name="email" placeholder="email address " />
 				<input type="text" name="name" placeholder="name" />
 				<textarea name="message"></textarea>
 				<input type="hidden" name="_subject" value="New submission!" />
 				<input type="text" name="_gotcha" style={{"display":"none"}} />
+				<input type="hidden" name="_next" value="/contact" />
 				<div className="buttonDiv">
 					<input type="submit" value="send" />
 				</div>
