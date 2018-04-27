c1b9bf92d9534edeac5246d097ff1843c32084db
ted <ted@warpedpuppy.com>
2018-04-27 07:50:47 +0100

    changed home page text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,12 +18,13 @@ class Home extends React.Component {
 			<HomeCanvas />
 			<div className="textCont">
 			<p>Hello!</p>
-            <p>My name is Ted and I'm a web developer.</p>
+            <p>My name is Ted and I&apos;m a web developer.</p>
             <p>I have 10+ experience in a variety of languages.  Most recently though, I&apos;ve been spending my time in React and Vue.</p>
             <p>I&apos;m lonely, contact me: <a href="mailto:ted@warpedpuppy.com">ted@warpedpuppy.com</a>
             </p>
             <p><a href="https://github.com/warpedpuppy" rel="noopener noreferrer" target="_blank">github</a> </p>
              <p><a href="https://www.linkedin.com/in/ted-walther-98926a8/" rel="noopener noreferrer" target="_blank">linkedin</a> </p>
+             <p class="small_italic">(In case you&apos;re interested, I just started building a fun site over <a href="//tugtug.com" rel="noopener noreferrer" target="_blank">here</a>)</p>
 		      <LogoWithBorder w="250" rotate="rotate(15)" color={true} />
 		     </div>
 	      </div>
