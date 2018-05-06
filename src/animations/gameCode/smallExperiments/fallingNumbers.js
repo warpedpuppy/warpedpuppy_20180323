2e211d4342c215e25b45d121e97e852f15c5cc4a
ted <ted@warpedpuppy.com>
2018-05-06 08:01:47 +0100

    cleaned green numbers code

--- src/animations/gameCode/smallExperiments/fallingNumbers.js
+++ src/animations/gameCode/smallExperiments/fallingNumbers.js
@@ -9,23 +9,36 @@ export default function Numbers (PIXI, Utils, Stats, loader_data) {
         streamerQ: 0,
         noDups: [],
         stats: new Stats(),
+        canvasHeight: 400,
         init: function () {
             this.canvasWidth = this.utils.returnCanvasWidth();
-            this.canvasHeight = 400;
             this.halfHeight = this.canvasHeight / 2;
             this.halfWidth = this.canvasWidth / 2;
             this.renderer = PIXI.autoDetectRenderer(this.canvasWidth, this.canvasHeight);
             document.getElementById("tugtugCanvas").appendChild(this.renderer.view);
+
             this.webGL = (this.renderer instanceof PIXI.CanvasRenderer) ? false : true;
+
             this.resize = this.resize.bind(this);
             window.onresize = this.resize;
+
+            this.Main = this.Main.bind(this);
+            this.animate = this.animate.bind(this);
+
+            this.app.ticker.add(this.animate);
+
+            this.stats.setMode(0);
+            let customContainer = document.getElementById('my-stats-container');
+            customContainer.appendChild(this.stats.domElement);
+
             if (!this.loader.resources.Typewriter) {
-              this.loader.add('Typewriter', "/fonts/games/fallingNumbers/greenNumbers.xml").load(this.Main.bind(this));
-               this.loader.onComplete.add(() => {
-                loader_data('off');
-                });
+              this.loader
+                  .add('Typewriter', "/fonts/games/fallingNumbers/greenNumbers.xml")
+                  .load(this.Main)
+                  .onComplete.add(() => {
+                      loader_data('off');
+                   });
             } else {
-              this.Main.bind(this)
               this.Main();
               loader_data('off')
             }
@@ -33,25 +46,27 @@ export default function Numbers (PIXI, Utils, Stats, loader_data) {
         },
         Main: function () {
             this.build();
-            this.stats.setMode(0);
-            let customContainer = document.getElementById('my-stats-container');
-            customContainer.appendChild(this.stats.domElement);
-            this.app.ticker.add(this.animate.bind(this));
         },
         build: function () {
-            let text0 = new PIXI.extras.BitmapText("0", {font: "12px Typewriter", align: "left"});
-            let width = text0.textWidth;
-            let height = text0.textHeight;
+            let textObj = {font: "12px Typewriter", align: "left"},
+                text0 = new PIXI.extras.BitmapText("0", textObj),
+                width = text0.textWidth,
+                height = text0.textHeight,
+                row,
+                col,
+                i,
+                j,
+                str,
+                testString;
             this.colQ = Math.ceil(this.canvasWidth/width);
             this.rowQ = Math.ceil(this.canvasHeight/height);
-            let row, col;
             this.noDups = [];
             this.streamers = [];
-            for(let i = 0; i < this.rowQ; i++){
+            for (i = 0; i < this.rowQ; i++) {
                 this.texts[i] = {};
-                for(let j = 0; j < this.colQ; j++){
-                    let str = (Math.floor(Math.random()*2) === 0)?"0":"1";
-                    text0 = new PIXI.extras.BitmapText(str, {font: "12px Typewriter", align: "left"});
+                for (j = 0; j < this.colQ; j++) {
+                    str = (Math.floor(Math.random()*2) === 0)?"0":"1";
+                    text0 = new PIXI.extras.BitmapText(str, textObj);
                     text0.x = width * j;
                     text0.y = height * i;
                     this.texts[i][j] = text0;
@@ -59,12 +74,13 @@ export default function Numbers (PIXI, Utils, Stats, loader_data) {
                     this.stage.addChild(text0);
                 }
             }
+
             this.streamerQ = Math.floor(this.colQ * 0.33);
 
-            for(let i = 0; i < this.streamerQ; i ++){
+            for(i = 0; i < this.streamerQ; i ++){
                 row = this.utils.randomIntBetween(0, this.rowQ-1);
                 col = this.utils.randomIntBetween(0, this.colQ-1);
-                let testString = `${row}_${col}`;
+                testString = `${row}_${col}`;
                 while(this.noDups.indexOf(testString) !== -1){
                      row = this.utils.randomIntBetween(0, this.rowQ-1);
                      col = this.utils.randomIntBetween(0, this.colQ-1);
@@ -75,13 +91,13 @@ export default function Numbers (PIXI, Utils, Stats, loader_data) {
             }
         },
         stop: function () {
+            window.onresize = undefined;
             this.loader.destroy();
             this.app.ticker.destroy();
             this.renderer.destroy();
         },
         resize: function () {
             this.canvasWidth = this.utils.returnCanvasWidth();
-            this.canvasHeight = 400;
             this.halfHeight = this.canvasHeight / 2;
             this.halfWidth = this.canvasWidth / 2;
             this.renderer.resize(this.canvasWidth,this.canvasHeight);
@@ -89,7 +105,9 @@ export default function Numbers (PIXI, Utils, Stats, loader_data) {
             this.build();
         },
         Streamer: function (col, row) {
-            let obj = {};
+            let obj = {},
+                lighted_number,
+                i;
             obj.counter = 0;
             obj.lit = [];
             obj.col = col;
@@ -111,8 +129,8 @@ export default function Numbers (PIXI, Utils, Stats, loader_data) {
                         this.newNumber.alpha = 1;
                         this.lit.push(this.newNumber);
                        
-                        for (let i = 0; i < this.lit.length; i ++) {
-                            let lighted_number = this.lit[i];
+                        for (i = 0; i < this.lit.length; i ++) {
+                            lighted_number = this.lit[i];
                             if (lighted_number.alpha > 0) {
                                 lighted_number.alpha -= .074;
                             } else {
