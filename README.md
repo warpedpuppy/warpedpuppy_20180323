962ea68e019fc2c793b3aeb66d7df890ab4c085a
ted <ted@warpedpuppy.com>
2020-01-21 08:35:18 -0500

    new home page info

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Redoing my personal site in React
+# My portfolio site
 
 > This site is up and visible [here](http://warpedpuppy.com>www.warpedpuppy.com).
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Home.css';
 import HomeCanvas from '../components/HomeCanvas';
 import LogoWithBorder from '../svgs/LogoWithBorder';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 class Home extends React.Component {
 	render () {
@@ -11,14 +11,14 @@ class Home extends React.Component {
 			<HomeCanvas />
 			<div className="textCont">
 			<p>Hello!</p>
-            <p>My name is Ted and I&apos;m a web developer.</p>
-            <p>I have 10+ experience in a variety of languages.  Most recently though, I&apos;ve been spending my time in React and Vue.</p>
+            <p>My name is Ted and I&apos;m a full stack web developer. I focus in a React front end (although I have experience with Vue) and a Node backend.</p>
+            <p>I have 10+ experience in a variety of languages.</p>
             <p>I&apos;m lonely, contact me: <a href="mailto:ted@warpedpuppy.com">ted@warpedpuppy.com</a>
             </p>
             <p><a href="https://github.com/warpedpuppy" rel="noopener noreferrer" target="_blank">github</a> </p>
              <p><a href="https://www.linkedin.com/in/e-w-98926a8/" rel="noopener noreferrer" target="_blank">linkedin</a> </p>
          
-             	<p></p>
+            
 		      <LogoWithBorder w="250" rotate="rotate(15)" color={true} />
 		     </div>
 	      </div>
